3febe6b4a3c8f189dae5d364b768b110686ef0b9
PratikshaSolat3987 <145910376+PratikshaSolat3987@users.noreply.github.com>
2025-04-11 09:03:57 +0530

    Add files via upload

--- src/main/controller/loginServlet.java
+++ src/main/controller/loginServlet.java
@@ -1,1 +1,37 @@
+package controller;
+
+import model.User;
+import util.HibernateUtil;
+import util.PasswordUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.io.IOException;
+
+public class loginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("FROM User WHERE email = :email", User.class);
+            query.setParameter("email", email);
+            User user = query.uniqueResult();
+
+            if (user != null && PasswordUtil.checkPassword(password, user.getPassword())) {
+                HttpSession httpSession = req.getSession();
+                httpSession.setAttribute("user", user);
+                resp.sendRedirect("displayStudent.html"); // or dashboard.html
+            } else {
+                resp.sendRedirect("login.html?error=invalid");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Login failed");
+        }
+    }
+}
 
